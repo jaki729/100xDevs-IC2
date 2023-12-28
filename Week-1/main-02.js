2e708e2752d84cd03654734d77e7b54b854bb108
Jakiur Rahman <jakiurrahman001@gmail.com>
2023-12-29 00:52:39 +0530

    update week-1

--- Week-1/main-02.js
+++ Week-1/main-02.js
@@ -136,3 +136,16 @@ fs.readFile("a.txt","utf-8",function(err,data) {
     console.log(data);
 });
 console.log("This is a message");
+
+// Asynch task example 
+function printHelloWorld() {
+    console.log("hello world")
+}
+
+setTimeout(printHelloWorld, 5000);
+
+let ans = 0;
+for (let i = 1; i<=5; i++) {
+    ans = ans + i
+}
+console.log(ans);
