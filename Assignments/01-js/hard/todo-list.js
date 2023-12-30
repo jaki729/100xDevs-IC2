3698bf74b73fb0b1f3668dc7fcefdf35485850dc
Jakiur Rahman <jakiurrahman001@gmail.com>
2023-12-30 20:23:22 +0530

    Update Week-1

--- Assignments/01-js/hard/todo-list.js
+++ Assignments/01-js/hard/todo-list.js
@@ -11,7 +11,38 @@
 */
 
 class Todo {
+  constructor() {
+    this.list = []; // Initializes an empty array to store todos
+  }
 
+  add(todo) {
+    this.list.push(todo); // Adds a new todo to the end of the list
+  }
+
+  remove(index) {
+    this.list.splice(index, 1); // Removes a todo at the specified index from the list
+  }
+
+  update(index, updatedTodo) {
+    // Updates the todo at the given index with the provided updatedTodo
+    if (index >= this.list.length) return; // Checks if the index is within bounds
+    this.list[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.list; // Returns the entire list of todos
+  }
+
+  get(index) {
+    // Retrieves the todo at the specified index
+    if (index >= this.list.length) return null; // Checks if the index is within bounds
+    return this.list[index];
+  }
+
+  clear() {
+    this.list = []; // Clears the entire list of todos by assigning an empty array
+  }
 }
+
 
 module.exports = Todo;
